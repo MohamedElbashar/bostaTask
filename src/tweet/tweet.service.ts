fefc8b515d7b04b21077490f86c5607a0f99466c
Mohamed Bashar <mohamedbashar200@gmail.com>
2021-05-31 18:19:12 +0200

    update

--- src/tweet/tweet.service.ts
+++ src/tweet/tweet.service.ts
@@ -1,17 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { Int } from '@nestjs/graphql';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { Tweet } from './dto/input/tweet.input';
+import { errorMessage } from 'src/users/dto/shared/errorMessage';
+import { ErrorResponse } from 'src/users/dto/shared/errorResponse';
+import { AddTweet } from './dto/input/tweet.input';
 
 @Injectable()
 export class TweetService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async addTweet(tweet: Tweet, userId: number): Promise<any> {
-    const createTweet = await this.prisma.tweet.create({
-      data: { content: tweet.content, userId: userId },
+  async getAllTweets(
+    tweet: AddTweet,
+    user_id: number,
+  ): Promise<ErrorResponse[] | null> {
+    console.log('hi tweet');
+    const createdTweet = await this.prisma.tweet.create({
+      data: { content: tweet.content, userId: user_id },
     });
+    if (!createdTweet)
+      return errorMessage('username', 'Invalid username or password');
+  }
 
-    return createTweet;
+  async addTweet(
+    tweet: AddTweet,
+    user_id: number,
+  ): Promise<ErrorResponse[] | null> {
+    console.log('hi tweet');
+    const createdTweet = await this.prisma.tweet.create({
+      data: { content: tweet.content, userId: user_id },
+    });
+    if (!createdTweet)
+      return errorMessage('username', 'Invalid username or password');
   }
 }
